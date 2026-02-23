43ae22f0adbadd2368904460158a8742696de68d
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-02-23 10:45:35 +0530

    Time: 0 ms (100%), Space: 17.5 MB (87.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans = {-1,-1};
-        bool visited = false;
-        int curridx = -1;
-        int lastidx = -1;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target && visited == false){
-                ans[0] = i;
-                visited = true;
-                curridx = i;
-            }
-            if(nums[i] == target && visited){
-                curridx = i;
-                lastidx = max(curridx,lastidx);
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high-low) / 2;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                high = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
         }
-        ans[1] = lastidx;
+        low = 0; high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high-low) / 2;
+            if(nums[mid] == target){
+                ans[1] = mid;
+                low = mid + 1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
         return ans;
     }
 };

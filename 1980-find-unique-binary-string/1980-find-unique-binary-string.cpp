24948e6ca7a98092f66fdfbbf9bc10417af3a3c3
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-03-08 16:23:56 +0530

    Time: 0 ms (100%), Space: 12.7 MB (72.81%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,41 +1,56 @@
+// optimised
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        vector<int> vals;
-        int ans;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int val = 0;
-            for (int j = 0; j < n; j++) {
-                val = (val << 1) | (nums[i][j] - '0');
-            }
-            vals.push_back(val);
+        string ans;
+        for(int i = 0; i < nums.size(); i++){
+            char ch = nums[i][i];
+            ans += (ch == '0'? '1' : '0');
         }
-
-        for (int i = 0; i < pow(2, n); i++) {
-            bool found = false;
-            for (int val : vals) {
-                if (val == i) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                ans = i;
-                break;
-            }
-        }
-
-        string a;
-        for (int i = 0; i < n; i++) {
-            if (ans & 1)
-                a.push_back('1');
-            else
-                a.push_back('0');
-            ans >>= 1;
-        }
-        reverse(a.begin(), a.end());
-        return a;
+        return ans;
     }
 };
+
+// SC - o(n)
+// tc - o(n^2)
+// class Solution {
+// public:
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         vector<int> vals;
+//         int ans;
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             int val = 0;
+//             for (int j = 0; j < n; j++) {
+//                 val = (val << 1) | (nums[i][j] - '0');
+//             }
+//             vals.push_back(val);
+//         }
+
+//         for (int i = 0; i < pow(2, n); i++) {
+//             bool found = false;
+//             for (int val : vals) {
+//                 if (val == i) {
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!found) {
+//                 ans = i;
+//                 break;
+//             }
+//         }
+
+//         string a;
+//         for (int i = 0; i < n; i++) {
+//             if (ans & 1)
+//                 a.push_back('1');
+//             else
+//                 a.push_back('0');
+//             ans >>= 1;
+//         }
+//         reverse(a.begin(), a.end());
+//         return a;
+//     }
+// };

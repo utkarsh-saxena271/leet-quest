a8d995eb454fe732ea684c7c82c65bb007d66f35
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-03-09 14:56:19 +0530

    Time: 0 ms (100%), Space: 13.7 MB (21.4%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,56 +1,25 @@
-// optimised
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        string ans;
-        for(int i = 0; i < nums.size(); i++){
-            char ch = nums[i][i];
-            ans += (ch == '0'? '1' : '0');
+        int n = nums.size();
+        string ans = "";
+        unordered_set<string> st(nums.begin(), nums.end());
+
+        return solve(0, "", n , st);
+    }
+
+    string solve(int i, string s , int n, unordered_set<string> &st){
+        if(i==n){
+            if(st.find(s) == st.end()){
+                return s;
+            }
+            return "";
         }
-        return ans;
+
+        string res = solve(i+1, s+'0', n , st);
+        if(!res.empty()) return res;
+
+        res = solve(i+1, s+'1', n , st);
+        return res;
     }
 };
-
-// SC - o(n)
-// tc - o(n^2)
-// class Solution {
-// public:
-//     string findDifferentBinaryString(vector<string>& nums) {
-//         vector<int> vals;
-//         int ans;
-//         int n = nums.size();
-//         for (int i = 0; i < n; i++) {
-//             int val = 0;
-//             for (int j = 0; j < n; j++) {
-//                 val = (val << 1) | (nums[i][j] - '0');
-//             }
-//             vals.push_back(val);
-//         }
-
-//         for (int i = 0; i < pow(2, n); i++) {
-//             bool found = false;
-//             for (int val : vals) {
-//                 if (val == i) {
-//                     found = true;
-//                     break;
-//                 }
-//             }
-
-//             if (!found) {
-//                 ans = i;
-//                 break;
-//             }
-//         }
-
-//         string a;
-//         for (int i = 0; i < n; i++) {
-//             if (ans & 1)
-//                 a.push_back('1');
-//             else
-//                 a.push_back('0');
-//             ans >>= 1;
-//         }
-//         reverse(a.begin(), a.end());
-//         return a;
-//     }
-// };

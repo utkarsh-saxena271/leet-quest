326c0e377b177f953693eb5c57ba19f04530372b
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-03-11 19:47:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.28%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        int count = 0;
+        while(n > 0){
+            if((n & 1) == 1){
+                count++;
+            }
+            n = n >> 1;
+            if(count > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+};

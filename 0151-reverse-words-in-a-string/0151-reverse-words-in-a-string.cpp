28ac87386a3f90e2f63a4ac9a23ce39510f8c8db
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-03-11 20:05:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,16 +3,15 @@ public:
     string reverseWords(string s) {
         string ans = "";
         reverse(s.begin(),s.end());
-
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.size(); i++){
             string word = "";
-            while(i < s.length() && s[i] != ' '){
+            while(i < s.size() && s[i] != ' '){
                 word += s[i];
                 i++;
             }
             reverse(word.begin(),word.end());
-            if(word.length() > 0){
-                ans+= ' '+  word;
+            if(word.size()>0){
+                ans+=' ' + word;
             }
         }
         return ans.substr(1);

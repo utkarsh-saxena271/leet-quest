23cca4e2a8d2468f4d4883aebc88b702d97a3e6e
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-04-03 09:58:18 +0530

    Time: 3 ms (65.2%), Space: 8.1 MB (18.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(n+1, -1);
-        
-        return solve(n,dp);
-    }
-    int solve(int n, vector<int>& dp){
-        if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        if(n<=1)return n;
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 };

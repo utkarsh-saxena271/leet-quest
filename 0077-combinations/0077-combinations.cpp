d0bb59ccb999b76b75bc42264d5dbea2d45067e2
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-04-04 09:33:02 +0530

    Time: 35 ms (91.25%), Space: 62.5 MB (83.3%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void findCombs(vector<int> nums, int k, int i, vector<int>& ds,
+    void findCombs(int n, int k, int i, vector<int>& ds,
                    vector<vector<int>>& ans) {
         if (k == 0) {
             ans.push_back(ds);
             return;
         }
-        for (int j = i; j <= nums.size()-k; j++) {
-            ds.push_back(nums[j]);
-            findCombs(nums, k - 1, j + 1, ds, ans);
+        for (int j = i; j <= n-k+1; j++) {
+            ds.push_back(j);
+            findCombs(n, k - 1, j + 1, ds, ans);
             ds.pop_back();
         }
     }
 
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums(n);
-        iota(nums.begin(), nums.end(), 1);
         vector<vector<int>> ans;
         vector<int> ds;
-        findCombs(nums, k, 0, ds, ans);
+        findCombs(n, k, 1, ds, ans);
         return ans;
     }
 };

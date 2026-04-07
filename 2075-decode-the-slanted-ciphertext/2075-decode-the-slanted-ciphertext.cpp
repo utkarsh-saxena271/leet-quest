872bd082a68497681defa61b8bb6ca5ad48dd11c
Utkarsh Saxena <utkarshsaxena271@gmail.com>
2026-04-07 20:06:42 +0530

    Time: 42 ms (28.72%), Space: 46.2 MB (27.52%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
         int l = encodedText.size();
-        int columns = l / rows;
+        int cols = l/rows;
         string originalText;
-        for (int col = 0; col < columns; col++) {
-            for (int j = col; j < l; j += (columns + 1)) {
+        for(int col = 0; col < cols; col++){
+            for(int j = col; j < l; j += (cols+1)){
                 originalText += encodedText[j];
             }
         }
-
         while (!originalText.empty() && originalText.back() == ' ') {
             originalText.pop_back();
         }
-        
         return originalText;
     }
 };
